ddcf6557c2c4da35406ef7648a2f9bd041da56d6
Will <32254701+willseff@users.noreply.github.com>
2022-03-29 10:06:10 -0400

    top100outcomes

--- Hs Rank vs Draft/script.R
+++ Hs Rank vs Draft/script.R
@@ -38,16 +38,49 @@ for (i in 2007:2021) {
   
 }
 
+rankings$PLAYER <- gsub("Otto Porter", "Otto Porter Jr.", rankings$PLAYER)
+
+##### Left join draftees with rankings #####
 
 joined.data <- left_join(draftees,rankings, by = c("Player" = "PLAYER"))
-
-joined.data %>% mutate(Pk = as.numeric(Pk),
-                       rank.diff = RK - Pk)
 
 # Get rid of duplicates
 subset(data.frame(table(joined.data$Player) >1),table.joined.data.Player....1==TRUE)
 
 joined.data <- joined.data[-c(209,77,558,647,678,339),]
+
+joined.data <- joined.data %>% mutate(Pk = as.numeric(Pk),
+                                      rank.diff = RK - Pk)
+
+sum <- joined.data %>% group_by(SCHOOL) %>% summarise(sum = sum(rank.diff), count = n()) %>% drop_na()
+sum <- sum %>% mutate(avg = sum/ count)
+joined.data %>% subset(SCHOOL == "Boston College")
+
+rankings %>% subset(SCHOOL == "Boston College")
+
+
+##### Left join rankings with draftees #####
+
+joined.data2 <- left_join(rankings,draftees, by = c("PLAYER" = "Player"))
+
+joined.data2 <- joined.data2 %>% group_by(SCHOOL) %>% count(is.na(Pk)) 
+
+colnames(joined.data2) <- c("School", "Drafted", "n")
+
+top100draftoutcomes <- full_join(subset(joined.data2, Drafted == TRUE), 
+                                 subset(joined.data2, Drafted == FALSE),
+                                 by = c('School' = 'School')) %>%
+  rename(Undrafted = n.x ,Drafted = n.y) %>% select(-c(Drafted.x, Drafted.y)) %>%
+  replace_na(list(Drafted = 0)) %>% mutate(Percentage.Drafted = Drafted/(Drafted+Undrafted))
+
+
+subset(joined.data, SCHOOL == "Georgetown")
+
+
+
+
+
+
 
 
 
