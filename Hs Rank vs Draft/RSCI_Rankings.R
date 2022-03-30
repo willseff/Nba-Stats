7114182480a13a7caf73c40635988adac2fff989
Will <32254701+willseff@users.noreply.github.com>
2022-03-30 00:19:30 -0400

    Update RSCI_Rankings.R

--- Hs Rank vs Draft/RSCI_Rankings.R
+++ Hs Rank vs Draft/RSCI_Rankings.R
@@ -1,3 +1,6 @@
+library(dplyr)
+library(ggplot2)
+
 
 rankings <- data.frame(RSCI = numeric(0),
                        Player = character(0),
@@ -5,15 +8,60 @@ rankings <- data.frame(RSCI = numeric(0),
                        College = character(0))
 
 
+for(i in 2007:2020){
+  
+  url <- read_html(paste0("https://www.basketball-reference.com/awards/recruit_rankings_", i,".html"))
+  dat <- html_table(url, header=FALSE)[[1]]
+  colnames(dat) <- dat[2,]
+  dat <- dat %>% select(RSCI,Player,Pk, College) %>% tail(nrow(dat)-2)
+  dat <- dat %>% mutate(College = ifelse(College == "", "No College", College),
+                        Player = gsub("(college)","",Player, fixed = TRUE),
+                        RSCI = as.numeric(gsub("T","",RSCI)),
+                        Pk = as.numeric(Pk)) %>%
+    subset(RSCI != "RSCI" & RSCI != "")
+  
+  rankings <- rbind(rankings,dat)
+}
+
+avg.draft.pos <- rankings %>% group_by(RSCI) %>% summarise(average = mean(Pk, na.rm = TRUE))
+
+rankings.drafted <- rankings %>% mutate(drafted = !is.na(Pk))
+
+rankings.drafted.grouped <- rankings.drafted %>% 
+  group_by(RSCI) %>% 
+  summarise(average.draft.spot = mean(Pk, na.rm = TRUE), 
+            number.drafted = sum(drafted), 
+            total = n()) %>%
+  mutate(draft.rate = number.drafted/total)
 
 
-url <- read_html("https://www.basketball-reference.com/awards/recruit_rankings_2020.html")
-dat <- html_table(url, header=FALSE)[[1]]
-colnames(dat) <- dat[2,]
-dat <- dat %>% select(RSCI,Player,Pk, College) %>% tail(nrow(dat)-2)
-dat <- dat %>% mutate(College = ifelse(College == "", "No College", College),
-               Player = gsub("(college)","",Player, fixed = TRUE),
-               RSCI = gsub("T","",RSCI)) %>%
-  subset(RSCI != "RSCI" & RSCI != "")
+# percent drafted by college
 
-rankings <- rbind(rankings,dat)
+drafted.rate.college <- rankings.drafted %>% 
+  group_by(College) %>% 
+  summarise(draft.rate = sum(drafted)/n(), n = n())
+
+# weighted draft score
+
+draft.joined <- rankings.drafted.grouped %>% 
+  select(RSCI,draft.rate) %>% 
+  right_join(rankings.drafted,
+             by = c("RSCI" = "RSCI")) %>% mutate(weighted.draft.score = drafted-draft.rate)
+
+# weighted draft score by college
+
+draft.score <- draft.joined %>% 
+  group_by(College) %>% 
+  summarise(draft.score.total = sum(weighted.draft.score),
+            n = n()) %>% mutate(draft.score.avg = draft.score.total/n)
+
+draft.score.subset <- draft.score %>% subset(n>10)
+
+
+# 
+
+rankings.drafted.grouped
+
+
+
+
